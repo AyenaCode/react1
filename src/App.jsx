eea980e96924655899c3bb4a655ffe7ddb480277
AyenaCode <ayenacode1@gmail.com>
2024-09-07 17:28:33 -0400

    setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-blue-500 card">react1</h1>
+      <button className="btn btn-primary">click me</button>
+    </div>
+  );
+};
+
+export default App;
